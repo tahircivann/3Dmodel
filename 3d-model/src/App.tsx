a7cb44505bee2c7fd97ce07df54e7db102086b2c
Tahir Civan <tahir.civan@samsonvt.com>
2023-06-07 01:06:20 +0200

    Add drag and drop and Annotation on model

--- 3d-model/src/App.tsx
+++ 3d-model/src/App.tsx
@@ -1,47 +1,121 @@
-/* eslint-disable */
-import * as THREE from 'three'
-import * as React from 'react'
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Stats, OrbitControls } from "@react-three/drei";
+import * as THREE from 'three';
+import * as React from 'react';
+import { useRef, useState, Suspense, useEffect } from 'react';
+import { Canvas, useFrame, useThree, extend } from "@react-three/fiber";
+import { Stats, OrbitControls, useGLTF, Loader } from "@react-three/drei";
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
+import { useDropzone } from 'react-dropzone';
+
+
+function Annotation() {
+  const { scene, camera, raycaster, mouse } = useThree();
+  const points = useRef<THREE.Points>();
+
+
+  useEffect(() => {
+    const geometry = new THREE.BufferGeometry();
+    points.current = new THREE.Points(geometry, new THREE.PointsMaterial({ color: 0xff0000 }));
+    scene.add(points.current);
+  }, [scene]);
+
+  useFrame(() => {
+    if (points.current) {
+      raycaster.setFromCamera(mouse, camera);
+      const intersects = raycaster.intersectObjects(scene.children);
+      if (intersects.length > 0) {
+        const point = intersects[0].point;
+        const positionAttr = new THREE.Float32BufferAttribute([point.x, point.y, point.z], 3);
+        points.current.geometry.setAttribute('position', positionAttr);
+        positionAttr.needsUpdate = true;
+      }
+    }
+  });
+
+  return null;
+}
 
 
 function Box(props: JSX.IntrinsicElements['mesh']) {
-  // This reference will give us direct access to the THREE.Mesh object
   const ref = useRef<THREE.Mesh>(null!)
-  // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+  useFrame(() => (ref.current.rotation.x += 0.01))
 
   return (
     <mesh
       {...props}
       ref={ref}
       scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
+      onClick={() => click(!clicked)}
+      onPointerOver={() => hover(true)}
+      onPointerOut={() => hover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
     </mesh>
   )
 }
 
+const GLTFDropzone: React.FC = () => {
+  const [modelUrl, setModelUrl] = useState<string | null>(null);
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { gltf: ['.gltf'], glb: ['.glb'] },
+    onDrop: ([file]) => {
+      const url = URL.createObjectURL(file);
+      setModelUrl(url);
+    },
+  });
+
+  return (
+    <>
+      <div {...getRootProps()} style={{
+        height: 100,
+        width: 100,
+        border: '1px solid black',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <input {...getInputProps()} />
+        Drop GLTFhere
+      </div>
+      {modelUrl && (
+        <Canvas style={{ height: '100vh', width: '100vw' }} camera={{ position: [5, 5, 5] }}>
+          <Stats />
+          <Suspense fallback={null}>
+            <Model url={modelUrl} />
+            <Annotation />
+            <OrbitControls />
+            <ambientLight intensity={0.5} />
+            <spotLight intensity={0.8} position={[300, 300, 4000]} />
+            <axesHelper args={[100]}
+              position={[0, 0, 0]}
+              scale={[10, 10, 10]}
+              up={new THREE.Vector3(0, 0, 1)}
+            />
+            <gridHelper args={[100, 100]}
+              position={[0, 0, 0]}
+              scale={[10, 10, 10]}
+              up={new THREE.Vector3(0, 0, 1)}
+            />
+          </Suspense>
+        </Canvas>
+      )}
+    </>
+  );
+};
+
+const Model: React.FC<{ url: string }> = ({ url }) => {
+  // Use URL string directly with useGLTF
+  const gltf: GLTF = useGLTF(url, false);
+  console.log(gltf);
+  return <primitive object={gltf.scene} dispose={null} />;
+};
+
 export default function App() {
   return (
-    <Canvas>
-      <Stats />
-      <OrbitControls />
-      <gridHelper />
-      <axesHelper />
-      <pointLight intensity={1.0} position={[5, 3, 5]} />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-    </Canvas>
-  )
+    <div>
+      <strong>Drop</strong> a glTF file into the box to load it.
+      <GLTFDropzone />
+    </div>
+  );
 }
